e5d29cc3bb6aa5ae64d56d35cfe74dda9e4276ab
urbancor <urban.cor@outlook.com>
2023-08-14 17:26:11 +0200

    add: insertion sort in python

---
+++ insertion_sort/insertion_sort.py
@@ -0,0 +1,11 @@
+def insertion_sort(array):
+    for i in range(1, len(array)):
+        j = 0
+        while array[j] < array[i] and j < i:
+            j += 1
+        while j <= i:
+            array[j], array[i] = array[i], array[j]
+            j += 1
+    return array
+
+print(insertion_sort([5, 4, 3, 2, 1])) # [1, 2, 3, 4, 5]
