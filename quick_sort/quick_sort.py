8fb5c752cf0dc040345a33118f1b2b1548c1c3ef
urbancor <urban.cor@outlook.com>
2023-08-22 15:09:09 +0200

    add: quicksort in python

---
+++ quick_sort/quick_sort.py
@@ -0,0 +1,10 @@
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr)//2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quick_sort(left) + middle + quick_sort(right)
+
+print(quick_sort([10, 22, 2, -1, 1, 4, 44])) # [-1, 1, 2, 4, 10, 22, 44]
